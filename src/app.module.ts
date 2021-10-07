ba48eadc475d1f2d9ef2597a2da560e9348a9d22
YuriiDzyuba <iurii11022@gmail.com>
2021-10-07 13:47:21 +0300

    added user controller module service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TagModule } from './tag/tag.module';
 import ormConfig from './ormConfig';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
-    imports: [TagModule, TypeOrmModule.forRoot(ormConfig)],
+    imports: [TagModule, TypeOrmModule.forRoot(ormConfig), UserModule],
     controllers: [AppController],
     providers: [AppService],
 })
